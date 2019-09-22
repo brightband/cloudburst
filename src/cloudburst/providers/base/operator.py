cf68d82dd38b62d69a38ad2bf7a0373ef0f0424b
cmcgrath13 <cpmcgrat@gmail.com>
2019-09-21 18:40:42 -0700

    Final structure

--- src/cloudburst/providers/base/operator.py
+++ src/cloudburst/providers/base/operator.py
@@ -1,5 +1,5 @@
 """
-Baseclass for an operation
+Baseclass for an operator
 
 Reqs:
     1) __repr__ should return an easy-to-understand string about the operation for a --dryrun
@@ -12,6 +12,6 @@ class OPCODES(object):
     START = None
      
 
-class Operation(object):
+class Operator(object):
     def __init__(self, resource: cloudburst.base.Resource, opcode: OPCODE):
         return
