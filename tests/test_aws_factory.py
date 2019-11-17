5feb8a646608695e1b4c3e8ae00dfa1bd6dbe278
cmcgrath13 <cpmcgrat@gmail.com>
2019-11-16 23:13:39 -0800

    Implement baseclass that no-ops if field does not exist

--- tests/test_aws_factory.py
+++ tests/test_aws_factory.py
@@ -7,7 +7,7 @@ from unittest import TestCase
 import json
 import os
 
-from cloudburst.utils.resource_factory import aws_factory
+from cloudburst.utils.resource_factory import aws_factory, Resource
 
 TEST_FILE = "test-fixtures/test_ec2.json"
 TEST_SERVICE = "EC2"
@@ -19,6 +19,8 @@ class TestAWSResourceFactory(TestCase):
         with open(os.path.join(MYPATH, TEST_FILE), "r") as tf:
             self._test_object = json.load(tf)
 
+        self.t = None
+
     def test_generate(self):
         t = aws_factory('ec2', self._test_object)
 
@@ -29,3 +31,15 @@ class TestAWSResourceFactory(TestCase):
         assert(t.VpcId == 'vpc-3f990858')
         assert(t.NetworkInterfaces[0].Status == "in-use")
         assert(t.NetworkInterfaces[0].PrivateIpAddresses[0].Association.PublicIp == "54.191.5.109")
+
+    def test_resource(self):
+        r = Resource()
+
+        assert(type(r.NotExists) == type(Resource()))
+        assert(type(r.NotExists.DefinitelyNotExists) == type(Resource()))
+        
+    def test_generate_not_exists(self):
+        t = aws_factory('ec2', self._test_object)
+
+        assert(type(t.NotExists) is type(Resource()))
+        assert(type(t.NotExists.DefinitelyNotExists) == type(Resource()))
