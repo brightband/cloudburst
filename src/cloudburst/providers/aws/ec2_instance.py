cab4aa99e010c6dd544085b5c23878349387c023
cmcgrath13 <cpmcgrat@gmail.com>
2019-11-18 15:31:19 -0800

    Multi-region support for EC2 Instances

--- src/cloudburst/providers/aws/ec2_instance.py
+++ src/cloudburst/providers/aws/ec2_instance.py
@@ -6,6 +6,7 @@ from cloudburst.providers.base import AWSService, opcode
 from cloudburst.utils.resource_factory import aws_factory 
 from cloudburst.utils.errors import NoResourcesError
 from cloudburst.utils.utils import aws_paginator
+from cloudburst.utils.shared_vars import AWS_REGIONS
 
 import boto3
 
@@ -19,7 +20,7 @@ class EC2Instance(AWSService):
     def __init__(self, session):
         self._session = session
         self._resources = []
-        self._client = None
+        self._client_map = {}
 
     @opcode
     def TERMINATE(self, resource):
@@ -42,39 +43,63 @@ class EC2Instance(AWSService):
         )
 
     @property
+    def supported_regions(self):
+        """
+        A property for defining the supported regions for a service. For EC2,
+        all regions are supported
+         
+        Returns:
+            aws_regions (dict of regions): Return the dictionary of supported regions
+                                           formatted as 'region identifier': 'human readable
+                                           name'. To be used while fetching and modifying
+                                           resources.
+        """
+        return AWS_REGIONS
+
+
+    @property
     def resources(self):
         """
         The property for returning/modifying resources for an EC2
         instance
+
+        Returns:
+            resources (list of Resource): A list of resource objects for the service
         """
         return self._resources
 
-    @property
-    def client(self):
+    def client(self, region=None):
         """
-        Lazy-load our client object
-        """
-        if self._client is None:
-            self._client = self._session.client('ec2')
-        return self._client
-
-    def fetch_all(self):
-        """
-        Fetch all EC2 instances from boto3
+        Return a client for the given region name, implemented this way to lazy-load clients
+        for each region to keep memory management intuitive
          
         Args:
-            N/A
+            region (string): The name of the region (region unique identifier e.g. 'us-west-2'). If
+                             None is provided, the default client will be fetched (region will be
+                             whatever is specified in credentials config)
          
         Returns:
-            N/A (Objects generated in this function are exposed through the resources property)
-         
-        Raises:
-            NoResourcesError: If no resources of the given type could be found
+            ec2client (boto3.client): The client to be used to make calls for a given region/service
         """
-        resps = aws_paginator(self.client.describe_instances)
+        # Check if the specified region name is in the client map, add if not exists
+        if region not in self._client_map.keys():
+            self._client_map[region] = None
+
+        # Check if the client map contains the client for a given region
+        if self._client_map[region] == None:
+            self._client_map[region] = self._session.client('ec2', region_name=region)
+
+        return self._client_map[region]
+
+    def _fetch_for_region(self, region):
+        """
+        Return a list of instance dictionaries for processing 
+        """
+        resps = aws_paginator(self.client(region).describe_instances)
 
         # Iterate over all fetched response objects from EC2, if we have
         # a lot of resources we'll have multiple responses due to pagination
+        instances = []
         for resp in resps:
             if (
                 "Reservations" in resp
@@ -89,18 +114,40 @@ class EC2Instance(AWSService):
                         and isinstance(reservation["Instances"], list)
                         and len(reservation["Instances"]) > 0
                     ):
-                        self._resource_factory(reservation["Instances"])
+                        instances += reservation["Instances"]
+        return instances
+
+
+    def fetch_all(self):
+        """
+        Fetch all EC2 instances from boto3
+         
+        Args:
+            N/A
+         
+        Returns:
+            N/A (Objects generated in this function are exposed through the resources property)
+         
+        Raises:
+            NoResourcesError: If no resources of the given type could be found
+        """
+        all_instances = []
+        for region in self.supported_regions:
+            regional_instances = self._fetch_for_region(region)
+            for instance in regional_instances:
+                instance["Region"] = region
+                self._resource_factory(instance)
 
         if len(self.resources) == 0:
             raise NoResourcesError("Could not find any resources of type {}".format(type(self).__name__))
 
-    def _resource_factory(self, resource_objs):
+    def _resource_factory(self, resource):
         """
-        Transform a list of resource dictionaries into resource objects and place them
-        under the resources property
+        Transform a resource dictionary into a resource object and place it under the resources
+        property
          
         Args:
-            resource_objs (list): A list of resource objects to transform
+            resource_objs (dict): A resource object to transform and add to the resources property
          
         Returns:
             N/A
@@ -108,7 +155,6 @@ class EC2Instance(AWSService):
         Raises:
             N/A
         """
-        for resource in resource_objs:
-            r = aws_factory(type(self).__name__, resource)
-            setattr(r, "__id__", r.InstanceId)
-            self._resources.append(r)
+        r = aws_factory(type(self).__name__, resource)
+        setattr(r, "__id__", r.InstanceId)
+        self._resources.append(r)
--- tests/test_ec2_instance.py
+++ tests/test_ec2_instance.py
@@ -6,6 +6,7 @@ import boto3
 import botocore
 
 from cloudburst.providers.aws import EC2Instance
+from cloudburst.utils.shared_vars import AWS_REGIONS
 
 TEST_FILE = "test-fixtures/test_responses.json"
 MYPATH = os.path.dirname(os.path.abspath(__file__))
@@ -14,18 +15,24 @@ MYPATH = os.path.dirname(os.path.abspath(__file__))
 class TestEC2Instance(TestCase):
     def setUp(self):
         sess = boto3.session.Session()
-        self.resource = EC2Instance(sess)
+        self.service = EC2Instance(sess)
 
     def test_resources(self):
-        assert self.resource.resources == []
+        assert self.service.resources == []
 
     def test_client(self):
-        assert type(self.resource.client).__name__ == 'EC2'
+        assert type(self.service.client()).__name__ == 'EC2'
+        assert type(self.service.client('us-west-2')).__name__ == 'EC2'
+
+    def test_supported_regions(self):
+        assert self.service.supported_regions == AWS_REGIONS
 
     @mock.patch('cloudburst.providers.aws.ec2_instance.aws_paginator')
     def test_fetch_all(self, paginator_mock):
         with open(os.path.join(MYPATH, TEST_FILE), 'r') as tf:
             paginator_mock.return_value = json.loads(tf.read())
 
-        self.resource.fetch_all()
-        assert isinstance(self.resource.resources, list)
+        self.service.fetch_all()
+        assert isinstance(self.service.resources, list)
+        # Test if region field is appropriately applied
+        getattr(self.service.resources[0], "Region")
